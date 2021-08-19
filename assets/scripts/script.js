959f666415b347c5787d4eb27177fab69294fa8d
Leonardo Ennes <leonardoennes7@gmail.com>
2021-08-19 19:34:53 -0300

    Primeiro commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const card = document.querySelector('.card');
+                              //eu mesmo fiz...
+function handleClick() {
+    card.classList.toggle('flip')
+}
+
+card.addEventListener('click', handleClick);
+
+
